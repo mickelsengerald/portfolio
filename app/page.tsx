8aba5f72e56db16eff587edf6e28aedf36961456
mickelsengerald <mickelsengerald@gmail.com>
2023-07-10 18:32:43 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl font-bold text-center">Portfolio currently under construction. Stay tuned!</h1>
+
+      {/* the rest of your code... */}
+
+    </main>
+  )
+}
